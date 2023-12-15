1b0539fbbac4ee2ce28577e9b75170c009c79e5c
Victor Saprykin <victanik@mail.ru>
2023-12-15 18:19:21 +0300

    Lesson 01 to check

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Ytllo");
+const title = "Learn JS";
+const screens = "Full";
+const screenPrice = 15;
+const rollback = true;
+const fullPrice = 1000;
+const adaptive = false;
+
+alert("We learn JS!");
+console.log("Work after learning!");
