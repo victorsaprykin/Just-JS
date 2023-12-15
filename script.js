79f2e2cc29ce20cec2a0d97a022f3742b2dde942
Victor Saprykin <victanik@mail.ru>
2023-12-15 19:11:38 +0300

    Lesson 02 1-t attempr

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const title = "Learn JS";
-const screens = "Full";
+const title = "Project";
+const screens = "Simple, Complex, Interactive";
 const screenPrice = 15;
 const rollback = true;
 const fullPrice = 1000;
 const adaptive = false;
 
-alert("We learn JS!");
-console.log("Work after learning!");
-
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
