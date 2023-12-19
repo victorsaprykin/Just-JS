2fc14acf48b6b2f80cec80970f32d9c20d293c9d
Victor Saprykin <victanik@mail.ru>
2023-12-19 12:08:14 +0300

    Fixed third дуыы 03

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ const title = prompt("Как называется Ваш проект?");
 
 const screens = prompt("Какие типы экранов нужно разработать?");
 
-const screenPrice = prompt("Сколько будет стоить данная работа?");
+const screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
 
 const adaptive = confirm("Нужен ли адаптив на сайте?");
 
 const addServise1 = prompt("Какой дополнительный тип услуги нужен?", "service1");
 
-const servicePrice1 = prompt("Сколько это будет стоить?");
+const servicePrice1 = Number(prompt("Сколько это будет стоить?"));
+console.log(servicePrice1);
 
 const addServise2 = prompt("Какой дополнительный тип услуги нужен?", "service2");
 
-const servicePrice2 = prompt("Сколько это будет стоить?");
+const servicePrice2 = Number(prompt("Сколько это будет стоить?"));
+
+console.log(servicePrice2);
 
 const fullPrice =
-  Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+  screenPrice + servicePrice1 + servicePrice2;
 console.log(fullPrice);
 const rollback = 5;
-const percentPrice = (fullPrice * rollback) / 100;
 
-const servicePercentPrice = Math.ceil(fullPrice - percentPrice);
+
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
 
 if (isNaN(servicePercentPrice)) {
   console.log("Косяк, однако!");
